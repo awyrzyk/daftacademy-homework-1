683d6e3acb067451f7b2188a2abac3b7749f52f7
A.Wyrzyk <alicjawyrzyk@gmail.com>
2019-03-24 16:53:31 +0100

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Daftacademy Homework nr 1'
+        })
+    ],    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
